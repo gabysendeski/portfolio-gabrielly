2afff8b514d4f2503e563b9498a02e27ee32b1c4
Gabrielly Sendeski <gaby.sendeski@hotmail.com>
2025-10-16 17:04:06 -0300

    feat: update Hero component with improved accessibility and image handling

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "/portfolio-gabrielly",
-  assetPrefix: "/portfolio-gabrielly/",
+  basePath: isProd ? "/portfolio-gabrielly" : "",
+  assetPrefix: isProd ? "/portfolio-gabrielly/" : "",
   images: {
     unoptimized: true,
   },
